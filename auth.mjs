ea69f8433e26f7744a083604d54a6f28649f361a
Anurag1096 <anuragqwerty09@outlook.com>
2025-06-20 11:36:16 +0530

    more commits

--- auth.mjs
+++ auth.mjs
@@ -1,41 +1,50 @@
-import fetch from 'node-fetch'
-import dotenv from 'dotenv';
+import fetch from "node-fetch";
+import dotenv from "dotenv";
 dotenv.config();
 
-const Auth_cred=async (token)=>{
-   try{
-    const endpoints = [
-  "access-check",
-  
-];
+const Auth_cred = async (auth_token, token) => {
+  try {
+    const endpoints = ["access-check"];
 
-const payload={
-  "name": "Anurag chakravarty",
-  "email": "anurag.011996@gmail.com",
-  "date": "2025-06-19 10:55:51"
-}
+    const payload = {
+      name: "Anurag chakravarty",
+      email: "anurag.011996@gmail.com",
+      date: "2025-06-19 15:39:18",
+    };
 
-for( let ep of endpoints){
-    const response= await fetch(`${process.env.API_URL_AUTH}${ep}`,{
-        method:"POST", 
+    for (let ep of endpoints) {
+      const response = await fetch(`${process.env.API_URL_AUTH}${ep}`, {
+        method: "GET",
         headers: {
-          Cookie: `auth_token=${token}`,
-          "Content-type":"application/json",
-      },
-      body:JSON.stringify(payload)
-    })
-    console.log(response)
-    console.log(`For endpoint ${ep} --> Response status ${response.status} :${response.statusText}`)
-    const data=await response.json();
-    console.log(data)
+          Authorization: `Bearer ${token}`,
+          Cookie: `auth_token=${auth_token}`,
+          Referer: "https://lucioai.com",
+          Origin: "https://lucioai.com",
+          "Content-Type": "application/json",
+        },
+        
+      });
+      console.log(JSON.stringify(payload));
+      console.log(response);
+      console.log(
+        `For endpoint ${ep} --> Response status ${response.status} :${response.statusText}`
+      );
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.log("Error occoured", error);
+  } finally {
+    console.log("Request finnished");
+  }
+};
+let token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQW51cmFnIGNoYWtyYXZhcnR5IiwiZW1haWwiOiJhbnVyYWcuMDExOTk2QGdtYWlsLmNvbSIsImRhdGUiOiIyMDI1LTA2LTE5IDE1OjM5OjE4In0.RTn1RqjYtu_t_jPzjP27zF3qBiWYi38L_ruDNjIF6Yg";
+let auth_token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQW51cmFnIGNoYWtyYXZhcnR5IiwiZW1haWwiOiJhbnVyYWcuMDExOTk2QGdtYWlsLmNvbSIsImRhdGUiOiIyMDI1LTA2LTE5IDE1OjM5OjE4In0.RTn1RqjYtu_t_jPzjP27zF3qBiWYi38L_ruDNjIF6Yg";
+if (token === auth_token) {
+  console.log("match");
+  Auth_cred(auth_token, token);
+} else {
+  console.log("does not match");
 }
-   }catch(error){
-    console.log("Error occoured",error)
-   }finally{
-    console.log("Request finnished")
-   }
-}
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQW51cmFnIGNoYWtyYXZhcnR5IiwiZW1haWwiOiJhbnVyYWcuMDExOTk2QGdtYWlsLmNvbSIsImRhdGUiOiIyMDI1LTA2LTE5IDEwOjU1OjUxIn0.Rm2oC_bi5J1GkAd_1eO46L-52sfYmeTMpxDbsZ8DPeg";
-
-Auth_cred(token);
--- post.mjs
+++ post.mjs
@@ -6,13 +6,11 @@ const payload={
     email:"anurag.011996@gmail.com"
 }
 
-const post_cred=async (payload,token)=>{
+const post_cred=async (payload)=>{
    try{
     const response= await fetch(process.env.API_URL,{
         method:"POST", headers: {
         "Content-Type": "application/json",
-        Cookie: `auth_token=${token}`,
-        
       },
         body:JSON.stringify(payload)
     })
@@ -30,9 +28,7 @@ const post_cred=async (payload,token)=>{
 // The following is the response from the "POST" method
 // {
 //   message: "Okay great, show out your arm and take the entry stamp. Also take this Authorization token, you'll need to show it to the bouncer as well to get in.",
-//   token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQW51cmFnIGNoYWtyYXZhcnR5IiwiZW1haWwiOiJhbnVyYWcuMDExOTk2QGdtYWlsLmNvbSIsImRhdGUiOiIyMDI1LTA2LTE5IDA3OjM5OjAyIn0.QBdrdUJyVksngxTZoJbrLErknyPw6dWAAP6AGzdSigc'
+// 
 // }
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQW51cmFnIGNoYWtyYXZhcnR5IiwiZW1haWwiOiJhbnVyYWcuMDExOTk2QGdtYWlsLmNvbSIsImRhdGUiOiIyMDI1LTA2LTE5IDEwOjU1OjUxIn0.Rm2oC_bi5J1GkAd_1eO46L-52sfYmeTMpxDbsZ8DPeg";
 
-post_cred(payload,token);
+post_cred(payload);
